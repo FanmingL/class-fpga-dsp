8fccf7d00052d1e67e7d163ef6464b07f96cc0c4
FanmingL <lfm19980917@outlook.com>
2018-04-26 17:37:30 +0800

    modify the fir filter parameter

--- project2/adc_control.v
+++ project2/adc_control.v
@@ -16,38 +16,40 @@ reg [7:0]step;
 //these fir parameter is 1e5 more than the original one.
 initial begin
 	ratio <= 100000;
-	fir_filter[0] <= 10;fir_filter[1] <= 16;fir_filter[2] <= 22;fir_filter[3] <= 29;
-	fir_filter[4] <= 35;fir_filter[5] <= 42;fir_filter[6] <= 50;fir_filter[7] <= 58;
-	fir_filter[8] <= 65;fir_filter[9] <= 73;fir_filter[10] <= 79;fir_filter[11] <= 85;
-	fir_filter[12] <= 89;fir_filter[13] <= 91;fir_filter[14] <= 90;fir_filter[15] <= 86;
-	fir_filter[16] <= 78;fir_filter[17] <= 65;fir_filter[18] <= 48;fir_filter[19] <= 25;
-	fir_filter[20] <= -3;fir_filter[21] <= -36;fir_filter[22] <= -75;fir_filter[23] <= -118;
-	fir_filter[24] <= -165;fir_filter[25] <= -216;fir_filter[26] <= -269;fir_filter[27] <= -323;
-	fir_filter[28] <= -376;fir_filter[29] <= -427;fir_filter[30] <= -473;fir_filter[31] <= -513;
-	fir_filter[32] <= -545;fir_filter[33] <= -565;fir_filter[34] <= -573;fir_filter[35] <= -566;
-	fir_filter[36] <= -541;fir_filter[37] <= -498;fir_filter[38] <= -434;fir_filter[39] <= -348;
-	fir_filter[40] <= -240;fir_filter[41] <= -108;fir_filter[42] <= 46;fir_filter[43] <= 224;
-	fir_filter[44] <= 424;fir_filter[45] <= 644;fir_filter[46] <= 883;fir_filter[47] <= 1139;
-	fir_filter[48] <= 1409;fir_filter[49] <= 1689;fir_filter[50] <= 1977;fir_filter[51] <= 2267;
-	fir_filter[52] <= 2558;fir_filter[53] <= 2843;fir_filter[54] <= 3119;fir_filter[55] <= 3383;
-	fir_filter[56] <= 3629;fir_filter[57] <= 3853;fir_filter[58] <= 4054;fir_filter[59] <= 4226;
-	fir_filter[60] <= 4367;fir_filter[61] <= 4475;fir_filter[62] <= 4548;fir_filter[63] <= 4585;
-	fir_filter[64] <= 4585;fir_filter[65] <= 4548;fir_filter[66] <= 4475;fir_filter[67] <= 4367;
-	fir_filter[68] <= 4226;fir_filter[69] <= 4054;fir_filter[70] <= 3853;fir_filter[71] <= 3629;
-	fir_filter[72] <= 3383;fir_filter[73] <= 3119;fir_filter[74] <= 2843;fir_filter[75] <= 2558;
-	fir_filter[76] <= 2267;fir_filter[77] <= 1977;fir_filter[78] <= 1689;fir_filter[79] <= 1409;
-	fir_filter[80] <= 1139;fir_filter[81] <= 883;fir_filter[82] <= 644;fir_filter[83] <= 424;
-	fir_filter[84] <= 224;fir_filter[85] <= 46;fir_filter[86] <= -108;fir_filter[87] <= -240;
-	fir_filter[88] <= -348;fir_filter[89] <= -434;fir_filter[90] <= -498;fir_filter[91] <= -541;
-	fir_filter[92] <= -566;fir_filter[93] <= -573;fir_filter[94] <= -565;fir_filter[95] <= -545;
-	fir_filter[96] <= -513;fir_filter[97] <= -473;fir_filter[98] <= -427;fir_filter[99] <= -376;
-	fir_filter[100] <= -323;fir_filter[101] <= -269;fir_filter[102] <= -216;fir_filter[103] <= -165;
-	fir_filter[104] <= -118;fir_filter[105] <= -75;fir_filter[106] <= -36;fir_filter[107] <= -3;
-	fir_filter[108] <= 25;fir_filter[109] <= 48;fir_filter[110] <= 65;fir_filter[111] <= 78;
-	fir_filter[112] <= 86;fir_filter[113] <= 90;fir_filter[114] <= 91;fir_filter[115] <= 89;
-	fir_filter[116] <= 85;fir_filter[117] <= 79;fir_filter[118] <= 73;fir_filter[119] <= 65;
-	fir_filter[120] <= 58;fir_filter[121] <= 50;fir_filter[122] <= 42;fir_filter[123] <= 35;
-	fir_filter[124] <= 29;fir_filter[125] <= 22;fir_filter[126] <= 16;fir_filter[127] <= 10;
+	fir_filter[0] <= 37;fir_filter[1] <= 39;fir_filter[2] <= 41;fir_filter[3] <= 44;
+	fir_filter[4] <= 46;fir_filter[5] <= 48;fir_filter[6] <= 49;fir_filter[7] <= 50;
+	fir_filter[8] <= 50;fir_filter[9] <= 48;fir_filter[10] <= 45;fir_filter[11] <= 40;
+	fir_filter[12] <= 33;fir_filter[13] <= 23;fir_filter[14] <= 11;fir_filter[15] <= -5;
+	fir_filter[16] <= -24;fir_filter[17] <= -46;fir_filter[18] <= -72;fir_filter[19] <= -100;
+	fir_filter[20] <= -132;fir_filter[21] <= -165;fir_filter[22] <= -201;fir_filter[23] <= -238;
+	fir_filter[24] <= -275;fir_filter[25] <= -311;fir_filter[26] <= -345;fir_filter[27] <= -376;
+	fir_filter[28] <= -403;fir_filter[29] <= -424;fir_filter[30] <= -438;fir_filter[31] <= -442;
+	fir_filter[32] <= -437;fir_filter[33] <= -420;fir_filter[34] <= -390;fir_filter[35] <= -347;
+	fir_filter[36] <= -288;fir_filter[37] <= -213;fir_filter[38] <= -122;fir_filter[39] <= -14;
+	fir_filter[40] <= 110;fir_filter[41] <= 251;fir_filter[42] <= 409;fir_filter[43] <= 582;
+	fir_filter[44] <= 769;fir_filter[45] <= 969;fir_filter[46] <= 1180;fir_filter[47] <= 1400;
+	fir_filter[48] <= 1627;fir_filter[49] <= 1859;fir_filter[50] <= 2093;fir_filter[51] <= 2326;
+	fir_filter[52] <= 2556;fir_filter[53] <= 2779;fir_filter[54] <= 2993;fir_filter[55] <= 3195;
+	fir_filter[56] <= 3382;fir_filter[57] <= 3552;fir_filter[58] <= 3702;fir_filter[59] <= 3831;
+	fir_filter[60] <= 3936;fir_filter[61] <= 4016;fir_filter[62] <= 4070;fir_filter[63] <= 4098;
+	fir_filter[64] <= 4098;fir_filter[65] <= 4070;fir_filter[66] <= 4016;fir_filter[67] <= 3936;
+	fir_filter[68] <= 3831;fir_filter[69] <= 3702;fir_filter[70] <= 3552;fir_filter[71] <= 3382;
+	fir_filter[72] <= 3195;fir_filter[73] <= 2993;fir_filter[74] <= 2779;fir_filter[75] <= 2556;
+	fir_filter[76] <= 2326;fir_filter[77] <= 2093;fir_filter[78] <= 1859;fir_filter[79] <= 1627;
+	fir_filter[80] <= 1400;fir_filter[81] <= 1180;fir_filter[82] <= 969;fir_filter[83] <= 769;
+	fir_filter[84] <= 582;fir_filter[85] <= 409;fir_filter[86] <= 251;fir_filter[87] <= 110;
+	fir_filter[88] <= -14;fir_filter[89] <= -122;fir_filter[90] <= -213;fir_filter[91] <= -288;
+	fir_filter[92] <= -347;fir_filter[93] <= -390;fir_filter[94] <= -420;fir_filter[95] <= -437;
+	fir_filter[96] <= -442;fir_filter[97] <= -438;fir_filter[98] <= -424;fir_filter[99] <= -403;
+	fir_filter[100] <= -376;fir_filter[101] <= -345;fir_filter[102] <= -311;fir_filter[103] <= -275;
+	fir_filter[104] <= -238;fir_filter[105] <= -201;fir_filter[106] <= -165;fir_filter[107] <= -132;
+	fir_filter[108] <= -100;fir_filter[109] <= -72;fir_filter[110] <= -46;fir_filter[111] <= -24;
+	fir_filter[112] <= -5;fir_filter[113] <= 11;fir_filter[114] <= 23;fir_filter[115] <= 33;
+	fir_filter[116] <= 40;fir_filter[117] <= 45;fir_filter[118] <= 48;fir_filter[119] <= 50;
+	fir_filter[120] <= 50;fir_filter[121] <= 49;fir_filter[122] <= 48;fir_filter[123] <= 46;
+	fir_filter[124] <= 44;fir_filter[125] <= 41;fir_filter[126] <= 39;fir_filter[127] <= 37;
+
+
 
 
 end
@@ -90,6 +92,7 @@ always @ (negedge clk_high )begin
 	end
 	4:begin
 		dac_out <= (multi_buffer[0] + multi_buffer[1]) / ratio;
+		//dac_out <= adc_in;
 		step = 0;
 	end
 	endcase
