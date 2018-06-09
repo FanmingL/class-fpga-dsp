2fa0054157f87d154b53ef8477ba80e541a32a62
FanmingL <lfm19980917@outlook.com>
2018-06-09 18:49:36 +0800

    audio seperation based on independent analysis

---
+++ MP3Encoder/Release/bin/subdir_vars.mk
@@ -0,0 +1,18 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../bin/hex.cmd \
+../bin/run.cmd 
+
+OBJ_SRCS += \
+../bin/MP3Encodercfg.obj \
+../bin/MP3Encodercfg_c.obj \
+../bin/fft.obj \
+../bin/iva.obj \
+../bin/matrix.obj \
+../bin/my_sys.obj 
+
+
